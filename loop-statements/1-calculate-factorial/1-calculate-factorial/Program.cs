ed5b31e262918186154dd46bb71c42badff2ba86
Hanninenjp <jphanninen@gmail.com>
2017-02-15 14:01:26 +0200

    Ekoodi programming basics

--- loop-statements/1-calculate-factorial/1-calculate-factorial/Program.cs
+++ loop-statements/1-calculate-factorial/1-calculate-factorial/Program.cs
@@ -10,30 +10,47 @@ namespace _1_calculate_factorial
     {
         static void Main(string[] args)
         {
-            //Could be Calculating a factorial N!, Enter N:
-            Console.WriteLine("Calculating a factorial");
-            Console.Write("Enter a number: ");
+            Console.WriteLine("Calculate factorial N!");
+            Console.Write("Enter N: ");
             //Need to add check for the number, has to be positive and > 1 according to the specification
             //However factorial 0! is also defined according to the convention for an empty product
             //Therefore factorial 0! can be also calculated
             //See https://en.wikipedia.org/wiki/Factorial for further details
             string numberInput = Console.ReadLine();
-            int numberValue = Int32.Parse(numberInput);
-
-            //Following is just for development time checking
-            //Console.WriteLine("User input is: {0}", numberInput);
-            //Console.WriteLine("Converted number is: {0}", numberValue);
-            //Console.ReadLine();
-
-            //Following is not strictly following the right formula for calculating N, but should yield the right result!
-            //Cases 0! = 1, and 1! = 1 could be handled as exceptions with an if-statement and the logic could be written differently
-            //Rewrite this!
-            int factorialValue = 1;
-            for (int i = 1; i <= numberValue; i++)
+            int numberValue = 0; // Int32.Parse(numberInput);
+            //Convert number with some error handling, see https://msdn.microsoft.com/en-us/library/bb397679.aspx
+            try
             {
-                factorialValue *= i;
+                numberValue = Convert.ToInt32(numberInput);
             }
-            Console.WriteLine("Factorial {0}! equals {1}!", numberValue, factorialValue);
+            catch (FormatException e)
+            {
+                Console.WriteLine("Input string is not a sequence of digits.");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("The number cannot fit in an Int32.");
+            }
+
+            if (numberValue < 0)
+            {
+                //Error handling for negative numbers
+                Console.WriteLine("N must be 0 or a positive integer!", numberValue + 1);
+            }
+            else
+            {
+                Console.WriteLine("N={0}", numberValue);
+                //Calculate factorial N!
+                //Following is not strictly following the right formula for calculating N!, but yields the right result!
+                //Cases 0! = 1, and 1! = 1 could be handled as exceptions with an if-statement and the logic could be written differently
+                int factorialValue = 1;
+                for (int i = 1; i <= numberValue; i++)
+                {
+                    factorialValue *= i;
+                }
+                Console.WriteLine("Factorial {0}! equals {1}!", numberValue, factorialValue);
+            }
+
             Console.WriteLine("\n");
             Console.WriteLine("Press Enter to exit!");
             Console.ReadLine();
