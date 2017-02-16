e1104168aa3afc2147e49a9740e88427ca86a800
Hanninenjp <jphanninen@gmail.com>
2017-02-16 16:44:55 +0200

    Ekoodi programming basics

---
+++ functions/1-stars/1-stars/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lets print some stars!");
+
+            //Function returns number of asterisks to display, prompt is passed to the function
+            int starsNumber = GetNumericInput("Enter number of stars: ");
+
+            //Error handling
+            if (starsNumber == 0)
+            {
+                Console.WriteLine("Fine {0} stars are printed!", starsNumber);
+            }
+            else if (starsNumber < 0)
+            {
+                Console.WriteLine("Number {0} is negative and is not allowed!", starsNumber);
+            }
+            else
+            {
+                //Display asterisks
+                Console.WriteLine();
+                for (int i = 0; i < starsNumber; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("*");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to exit!");
+            Console.ReadLine();
+        }
+        public static int GetNumericInput(string prompt)
+        {
+            int numberValue = 0;
+            bool success = true;
+            do
+            {
+                //Prompt user
+                Console.Write(prompt);
+                //Read user input
+                string numberInput = Console.ReadLine();
+                //Make conversion and perform exception handling for the user input
+                try
+                {
+                    //Convert number
+                    //Conversion failures will throw an exception
+                    numberValue = Convert.ToInt32(numberInput);
+                    //Conversion is successful, else exception handling is performed
+                    //Done
+                    success = true;
+                }
+                catch (FormatException e)
+                {
+                    success = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is not a valid number!");
+                    Console.ResetColor();
+                }
+                catch (OverflowException e)
+                {
+                    success = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is out of range (1 - {0})!", int.MaxValue);
+                    Console.ResetColor();
+                }
+            } while (!success);
+            return numberValue;
+        }
+    }
+}
---
+++ functions/2-minimum/2-minimum/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_minimum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Find a minimum of set of two numbers!");
+
+            int firstNumber = GetNumericInput("Enter the first number: ");
+            int secondNumber = GetNumericInput("Enter the second number: ");
+
+            //Equality is not handled as a special case
+            int minNumber = GetMinValue(firstNumber, secondNumber);
+
+            Console.WriteLine("Minimum of {{{0},{1}}} is {2}).", firstNumber, secondNumber, minNumber);
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to exit!");
+            Console.ReadLine();
+        }
+        public static int GetMinValue(int firstValue, int secondValue)
+        {
+            return Math.Min(firstValue, secondValue);
+        }
+        public static int GetNumericInput(string prompt)
+        {
+            int numberValue = 0;
+            bool success = true;
+            do
+            {
+                //Prompt user
+                Console.Write(prompt);
+                //Read user input
+                string numberInput = Console.ReadLine();
+                //Make conversion and perform exception handling for the user input
+                try
+                {
+                    //Convert number
+                    //Conversion failures will throw an exception
+                    numberValue = Convert.ToInt32(numberInput);
+                    //Conversion is successful, else exception handling is performed
+                    //Done
+                    success = true;
+                }
+                catch (FormatException e)
+                {
+                    success = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is not a valid number!");
+                    Console.ResetColor();
+                }
+                catch (OverflowException e)
+                {
+                    success = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input is out of range (1 - {0})!", int.MaxValue);
+                    Console.ResetColor();
+                }
+            } while (!success);
+            return numberValue;
+        }
+    }
+}
