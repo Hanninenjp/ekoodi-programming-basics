a14270eb2b4f76ada29ca6f66ff92beadc8f3040
Hanninenjp <jphanninen@gmail.com>
2017-02-26 20:14:25 +0200

    Ekoodi programming basics

---
+++ object-oriented-basics/object-oriented-basics/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_basics
+{
+    class Car
+    {
+        private string _make;
+        private decimal _speed;
+
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+        }
+
+        public decimal Speed
+        {
+            get
+            {
+                return _speed;
+            }
+        }
+
+        public Car()
+        {
+            _make = "Ford";
+            _speed = 80;
+        }
+
+        public Car(string make, decimal speed)
+        {
+            _make = make;
+            _speed = speed;
+        }
+
+        public string ShowInformation()
+        {
+            return String.Format("\nMake: {0}\nCurrent speed: {1} km/h", _make, _speed);
+        }
+
+        public void Accelerate(decimal increase)
+        {
+            _speed += increase;
+        }
+
+        public void Decelerate()
+        {
+            _speed *= (decimal)0.9;
+        }
+    }
+}
---
+++ object-oriented-basics/object-oriented-basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Car> cars = new List<Car>();
+            cars.Add(new Car("Seat", 120));
+            cars.Add(new Car("Skoda", 100));
+            cars.Add(new Car("Volkswagen", 80));
+            Console.WriteLine("Cars are cruising on a highway!");
+            foreach(Car car in cars)
+            {
+                Console.WriteLine(car.ShowInformation());
+            }
+            decimal speedIncrease = 20;
+            Console.WriteLine("\nCars will now accelerate {0} km/h!", speedIncrease);
+            foreach (Car car in cars)
+            {
+                car.Accelerate(speedIncrease);
+            }
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ShowInformation());
+            }
+            Console.WriteLine("\nCars will now decelerate!");
+            foreach (Car car in cars)
+            {
+                car.Decelerate();
+            }
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ShowInformation());
+            }
+            Console.WriteLine("\nPress any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
